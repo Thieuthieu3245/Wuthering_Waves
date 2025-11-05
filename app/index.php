e559143cd3125498cc4882c933c93c78ff9e8abc
Thieuthieu De Rune <matthieu.thivard03@gmail.com>
2025-11-05 16:36:49 +0100

    Gérer la partie V du MVC

--- app/index.php
+++ app/index.php
@@ -6,4 +6,6 @@ $loader = new Helpers\Psr4AutoloaderClass;
 $loader->register();
 
 $loader->addNamespace('\Helpers', '/Helpers');
+$loader->addNamespace('\League\Plates', '/Vendor/Plates/src');
 
+$template = new \League\Plates\Engine(__DIR__ . '/Views');
