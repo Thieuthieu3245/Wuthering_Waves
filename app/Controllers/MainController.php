a7040fd3aa517e9f6dcdedb7b2dbe832867e3de2
Thieuthieu De Rune <matthieu.thivard03@gmail.com>
2025-11-05 16:37:19 +0100

    Gérer le contrôleur pour afficher la vue

---
+++ app/Controllers/MainController.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace Controllers;
+
+use League\Plates\Engine;
+
+class MainController 
+{
+    private Engine $templates;
+    
+    public function __construct(Engine $engine) {
+        $this->templates = $engine;
+    }
+
+    public function index():void {
+        echo $this->templates->render('home',['gameName'=>'Genshin Impact']);
+    }
+}
--- app/index.php
+++ app/index.php
@@ -7,5 +7,9 @@ $loader->register();
 
 $loader->addNamespace('\Helpers', '/Helpers');
 $loader->addNamespace('\League\Plates', '/Vendor/Plates/src');
+$loader->addNamespace('\Controllers', '/Controllers');
 
 $template = new \League\Plates\Engine(__DIR__ . '/Views');
+
+$controller = new \Controllers\MainController($template);
+$controller->index();
