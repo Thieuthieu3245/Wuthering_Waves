10c0ea33134be528fc37122d767a8f45b7458400
Thieuthieu De Rune <matthieu.thivard03@gmail.com>
2025-11-23 13:31:45 +0100

    Ajout des elements

---
+++ app/Models/ElementDAO.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Models;
+
+use Models\BasePDODAO;
+
+class ElementDAO extends BasePDODAO{
+
+    public function getAll(){
+        $sql = "SELECT * FROM element_";
+        $stmt = $this->execRequest($sql);
+        return $stmt->fetchAll();
+    }
+
+    public function getById(string $id){
+        $sql = "SELECT * FROM element_ WHERE idElement = ?";
+        $stmt = $this->execRequest($sql, [$id]);
+        return $stmt->fetch();
+    }
+
+    public function delete(string $id){
+        $sql = "DELETE FROM element_ WHERE idElement = ?";
+        $stmt = $this->execRequest($sql, [$id]);
+        return $stmt !== false;
+    }
+
+    public function create(Element $element){
+        $sql = "INSERT INTO element_ (idElement, Name, url_image) VALUES (?, ?, ?)";
+        $stmt = $this->execRequest($sql, [
+            $element->getId(),
+            $element->getName(),
+            $element->getUrlImg()
+        ]);
+        return $stmt !== false;
+    }
+
+    public function edit(Element $element){
+        $sql = "UPDATE element_ SET Name = ?, url_image = ? WHERE idElement = ?";
+        $stmt = $this->execRequest($sql, [
+            $element->getName(),
+            $element->getUrlImg(),
+            $element->getId()
+        ]);
+        return $stmt !== false;
+    }
+}
---
+++ app/Services/ElementService.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Services;
+
+use Models\Element;
+use Models\ElementDAO;
+
+class ElementService{
+
+    private $elementDAO;
+
+    public function __construct() {
+        $this->elementDAO = new ElementDAO();
+    }
+
+    public function getAllElements() {
+        $data = $this->elementDAO->getAll();
+        return array_map([$this, 'hydrate'], $data);
+    }
+
+    public function getElementById($id) {
+        $data = $this->elementDAO->getById($id);
+        return $this->hydrate($data);
+    }
+
+    public function deleteElement($id) {
+        return $this->elementDAO->delete($id);
+    }
+
+    public function createElement(Element $Element) {
+        return $this->elementDAO->create($Element);
+    }
+
+    public function editElement(Element $Element) {
+        return $this->elementDAO->edit($Element);
+    }
+
+    private function hydrate(array $data) : Element {
+        $Element = new Element($data['idElement'], $data['Name'], $data['url_image']);
+        return $Element;
+    }
+}
